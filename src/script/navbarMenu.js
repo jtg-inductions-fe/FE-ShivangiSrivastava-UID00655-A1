a33a9bc6058ca16bdb7ddb945aa85c89e61d9b44
Shivangi <shivangi.srivastava@joshtechnologygroup.com>
2025-09-19 15:55:48 +0530

    SS_A1_09: navbar fixes

--- src/script/navbarMenu.js
+++ src/script/navbarMenu.js
@@ -6,14 +6,17 @@ const navBtns = document.querySelector('#nav-btns');
 const navMenu = document.querySelector('#nav-menu');
 const mainSection = document.querySelector('#main-section');
 const footerSection = document.querySelector('#footer-section');
+const navBar = document.querySelector('.navbar');
 const ready = hamOpen && hamClose && navLinks && navBtns && navMenu;
+const navLogo = document.querySelector('#nav-logo');
+
+const isTablet = window.innerWidth >= 1024 && window.innerWidth < 1440;
 
 //Open/Close Navigation Sidebar
 function toggleMenu(isOpen) {
     if (!ready) {
         return;
     }
-    const isTablet = window.innerWidth >= 1024;
     navLinks.classList.toggle('active', isOpen);
     navMenu.setAttribute('aria-expanded', isOpen);
     hamOpen.classList.toggle('closed', isOpen);
@@ -21,11 +24,22 @@ function toggleMenu(isOpen) {
     document.body.classList.toggle('no-scroll', isOpen);
     mainSection.toggleAttribute('inert', isOpen);
     footerSection.toggleAttribute('inert', isOpen);
+    navBtns.classList.toggle('active', isOpen);
 
     if (isTablet) {
         navBtns.toggleAttribute('inert', isOpen);
     }
 }
+
+function reorderForTab() {
+    if (isTablet) {
+        navBar.insertBefore(navMenu, navLogo);
+    } else {
+        navBar.insertBefore(navLogo, navMenu);
+    }
+}
+
+reorderForTab();
 
 if (ready) {
     hamOpen.addEventListener('click', () => toggleMenu(true));
