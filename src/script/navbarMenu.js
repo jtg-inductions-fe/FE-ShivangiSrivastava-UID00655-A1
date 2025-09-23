dc55ab3583634e8b010e37d1db7a0ae13ede10ef
Shivangi <shivangi.srivastava@joshtechnologygroup.com>
2025-09-23 12:19:06 +0530

    SS_A1_09: Review fixes

--- src/script/navbarMenu.js
+++ src/script/navbarMenu.js
@@ -4,12 +4,10 @@ const hamClose = document.querySelector('#menu-close');
 const navLinks = document.querySelector('#navbar-list');
 const navBtns = document.querySelector('#nav-btns');
 const navMenu = document.querySelector('#nav-menu');
-const navContent = navLinks.parentElement;
 const mainSection = document.querySelector('#main-section');
 const footerSection = document.querySelector('#footer-section');
 const navBar = document.querySelector('.navbar');
 const ready = hamOpen && hamClose && navLinks && navBtns && navMenu;
-const navLogo = document.querySelector('#nav-logo');
 const isTablet = window.innerWidth >= 1024 && window.innerWidth < 1440;
 
 //Open/Close Navigation Sidebar
@@ -26,7 +24,10 @@ function toggleMenu(isOpen) {
     mainSection.toggleAttribute('inert', isOpen);
     footerSection.toggleAttribute('inert', isOpen);
     navBtns.classList.toggle('active', isOpen);
-
+    setTimeout(() => {
+        navLinks.classList.toggle('no-transition', !isOpen);
+        navBtns.classList.toggle('no-transition', !isOpen);
+    }, 500);
     if (isTablet) {
         navBtns.toggleAttribute('inert', isOpen);
     }
@@ -37,38 +38,39 @@ if (ready) {
     hamClose.addEventListener('click', () => toggleMenu(false));
 }
 
-//Reorder Tab for tablet view
-function reorderForTab() {
-    if (window.innerWidth >= 1024 && window.innerWidth < 1440) {
-        navContent.insertBefore(navMenu, navLogo);
-    } else {
-        navContent.insertBefore(navLogo, navMenu);
-    }
-}
+//Throttles handleScroll Function call
+const throttle = (fn, delay) => {
+    let inThrottle = false,
+        lastFn,
+        lastTime;
+    return function () {
+        if (!inThrottle) {
+            fn.apply(this, arguments);
+            lastTime = Date.now();
+            inThrottle = true;
+        } else {
+            clearTimeout(lastFn);
+            lastFn = setTimeout(
+                function () {
+                    if (Date.now() - lastTime >= delay) {
+                        fn.apply(this, arguments);
+                        lastTime = Date.now();
+                    }
+                },
+                delay - (Date.now() - lastTime),
+            );
+        }
+    };
+};
 
-//Prevent translate transitions during resize
-let resizeTime;
-function noTransition() {
-    navLinks.classList.add('no-transition');
-    navBtns.classList.add('no-transition');
-
-    clearTimeout(resizeTime);
-    resizeTime = setTimeout(() => {
-        navLinks.classList.remove('no-transition');
-        navBtns.classList.remove('no-transition');
-    }, 250);
-}
-
-window.addEventListener('resize', () => {
-    noTransition();
-    reorderForTab();
-});
-
-//Add box shadow on scroll
-window.addEventListener('scroll', () => {
-    if (scrollY > 0) {
+// Adds box shadow on scroll
+const handleScroll = () => {
+    if (window.scrollY > 0) {
         navBar.classList.add('scrolled');
     } else {
         navBar.classList.remove('scrolled');
     }
-});
+};
+
+// Add the throttled function to the scroll event listener
+window.addEventListener('scroll', throttle(handleScroll, 250));
