bda669e601d6e290abc5d9e035051db4bb33c3a4
Shivangi <shivangi.srivastava@joshtechnologygroup.com>
2025-09-23 15:50:18 +0530

    SS_A1_09: Review fixes: added documentation to JS

--- src/script/footerMenu.js
+++ src/script/footerMenu.js
@@ -1,9 +1,9 @@
-// Provides footer dropdown menu functionality on mobile
+// Provides footer dropdown menu functionality on mobile by expanding the according when icon is clicked
 document.querySelectorAll('.company-details__dropdown').forEach((dropdown) => {
     const container = dropdown.parentElement;
     const list = container.querySelector('.company-details__list');
     const icon = dropdown.querySelector('.company-details__icon');
-    icon.addEventListener('click', () => {
+    dropdown.addEventListener('click', () => {
         const isOpen = !container.classList.contains('is-open');
         list.classList.toggle('active', isOpen);
         container.classList.toggle('is-open', isOpen);
--- src/script/navbarMenu.js
+++ src/script/navbarMenu.js
@@ -8,9 +8,11 @@ const mainSection = document.querySelector('#main-section');
 const footerSection = document.querySelector('#footer-section');
 const navBar = document.querySelector('.navbar');
 const ready = hamOpen && hamClose && navLinks && navBtns && navMenu;
-const isTablet = window.innerWidth >= 1024 && window.innerWidth < 1440;
 
-//Open/Close Navigation Sidebar
+/**
+ * Toggles the visibility of navigation bar menu
+ * @param {boolean} isOpen - indicates where the menu should be open (true) or closed (false)
+ */
 function toggleMenu(isOpen) {
     if (!ready) {
         return;
@@ -28,49 +30,9 @@ function toggleMenu(isOpen) {
         navLinks.classList.toggle('no-transition', !isOpen);
         navBtns.classList.toggle('no-transition', !isOpen);
     }, 500);
-    if (isTablet) {
-        navBtns.toggleAttribute('inert', isOpen);
-    }
 }
 
 if (ready) {
     hamOpen.addEventListener('click', () => toggleMenu(true));
     hamClose.addEventListener('click', () => toggleMenu(false));
 }
-
-//Throttles handleScroll Function call
-const throttle = (fn, delay) => {
-    let inThrottle = false,
-        lastFn,
-        lastTime;
-    return function () {
-        if (!inThrottle) {
-            fn.apply(this, arguments);
-            lastTime = Date.now();
-            inThrottle = true;
-        } else {
-            clearTimeout(lastFn);
-            lastFn = setTimeout(
-                function () {
-                    if (Date.now() - lastTime >= delay) {
-                        fn.apply(this, arguments);
-                        lastTime = Date.now();
-                    }
-                },
-                delay - (Date.now() - lastTime),
-            );
-        }
-    };
-};
-
-// Adds box shadow on scroll
-const handleScroll = () => {
-    if (window.scrollY > 0) {
-        navBar.classList.add('scrolled');
-    } else {
-        navBar.classList.remove('scrolled');
-    }
-};
-
-// Add the throttled function to the scroll event listener
-window.addEventListener('scroll', throttle(handleScroll, 250));
